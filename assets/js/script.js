697ea3f408e0c93bc48ea6abd7c8d76f5c68b071
Brioni Connolly <brioniconnolly@gmail.com>
2023-05-22 16:48:00 +0000

    add styling to keyboard element and begin to wire it up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// getWord() function will be inseparate js file called words.js
+// getWord function will be inseparate js file called words
 // Pulling elements from the DOM to manipulate. image and lettertiles will correspond to each other.
 const image = document.querySelector("clue-image-container");
 const letterTiles = document.querySelector("letter-tile-container");
@@ -8,15 +8,29 @@ const keyboard = document.querySelector(".keyboard-key-container"); // Corrected
 // Leave it as a constant array for now but maight come back in and change it to a shuffle that contains only the keys contained in word
 
 const keys = [
-"a", "b", "c", "d","e","f", "g", "h","i", "j","k", "l","m", "n","o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "enter", "\b"
+"a", "b", "c", "d","e","f", "g", "h","i", "j","k", "l","m", "n","o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "enter", "<"
 ]
 
+// Here are the rows for attemptRows, these rows will be used to create letter tiles
+const attemptRows = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+]
+const handleClick = () => {
+  console.log("clicked")
+}
+
 // Create a button for each key
-keys.forEach(key => {
+keys.forEach (key => {
     const buttonObject = document.createElement("button");
     buttonObject.textContent = key;
-    buttonObject.classList.add("keyboard-key-container"); 
-    // Add a CSS class for styling
+    buttonObject.classList.add(".keyboard-key-container"); 
     keyboard.appendChild(buttonObject);
+    buttonObject.addEventListener("click", handleClick);
   });
   
+  attemptRows.forEach((attemptRow, attemptRowIndex) => {
+   const rowElement = document.createElement("div");
+   rowElement.setAttribute("id", "attemptRow" + attemptRowIndex)
+  })
